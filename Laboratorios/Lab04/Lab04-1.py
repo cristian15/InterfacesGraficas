833f938d07b5857c4a811a6408e560805a1d219f
cristian15 <cristian15002002@gmail.com>
2014-10-03 03:24:06 -0400

    .

--- Laboratorios/Lab04/Lab04-1.py
+++ Laboratorios/Lab04/Lab04-1.py
@@ -1,3 +1,7 @@
+# Profesor: Luis Caro Saldivia
+# Nombre: Cristian Beltran Concha
+# Carrera: Ing. civil Informatica UCT
+#====================================
 import pygame.image
 import PIL
 import win32com.client
