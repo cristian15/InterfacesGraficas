d97b772d6b0c677db3455e0ee8fd3c21a9fb05e0
cristian15 <cristian15002002@gmail.com>
2014-10-03 19:59:11 -0400

    avance

--- Laboratorios/Lab04/Lab04-3.py
+++ Laboratorios/Lab04/Lab04-3.py
@@ -9,11 +9,19 @@ scene.autocenter = True
 scene.autoscale = True
 scene.background = (255,255,255)
 #=================
-ground = box(pos=(0, 0, 0), size=(400, 6, 200), color = color.blue)	# piso en VPython
+ground = box(pos=(200, 0, 100), size=(400, 6, 200), color = color.blue)	# piso en VPython
 
 nR = 5 # numero de robots
 
-for i in range(0, nR):
-	x = RA.randint(10,200)
-	z = RA.randint(10,100)
-	robot[i] = cylinder(pos=(x,3,z),axis =(0,6,0),radius=10 , color = color.red)
+def movRobots():
+	
+	return
+robot = []
+for i in range(nR):
+	x = RA.randint(10,400-10)
+	z = RA.randint(10,200-10)
+	robot.append(cylinder(pos=(390,3,z),axis =(0,6,0),radius=10 , color = color.red))
+	robot[i].vel = RA.random()	# asigna velocidad
+	robot[i].theta = RA.randint(0,359)
+	
+	print robot[i].vel
