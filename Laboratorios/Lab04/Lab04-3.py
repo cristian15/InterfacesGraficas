dc5490026ffcf3e5ac837bb73fc62ba32f40a7a1
cristian15 <cristian15002002@gmail.com>
2014-10-04 01:12:11 -0400

    Avance

--- Laboratorios/Lab04/Lab04-3.py
+++ Laboratorios/Lab04/Lab04-3.py
@@ -1,4 +1,5 @@
 import random as RA
+import math
 from visual import *
 
 
@@ -9,19 +10,44 @@ scene.autocenter = True
 scene.autoscale = True
 scene.background = (255,255,255)
 #=================
+width = 400
+height = 200
 ground = box(pos=(200, 0, 100), size=(400, 6, 200), color = color.blue)	# piso en VPython
 
 nR = 5 # numero de robots
 
 def movRobots():
-	
+	for i in range(nR):
+		robot[i].x += math.cos(robot[i].theta)*robot[i].velocity
+		robot[i].z += math.sin(robot[i].theta)*robot[i].velocity
+	return
+def bounce():
+	for i in range(nR):
+		if robot[i].x >= width-robot[i].radius:
+			robot[i].x -=  (robot[i].x +robot[i].radius)-width
+			robot[i].theta = - robot[i].theta
+			print robot[i].theta
+		elif robot[i].x < robot[i].radius:
+			robot[i].x +=  robot[i].radius-robot[i].x
+			robot[i].theta = -robot[i].theta
+		if robot[i].z > height-robot[i].radius:
+			robot[i].z -=  (robot[i].z +robot[i].radius) -height
+			robot[i].theta = math.pi + robot[i].theta
+		elif robot[i].z < robot[i].radius:
+			robot[i].z +=  robot[i].radius-robot[i].z
+			robot[i].theta = math.pi - robot[i].theta
 	return
 robot = []
 for i in range(nR):
-	x = RA.randint(10,400-10)
-	z = RA.randint(10,200-10)
-	robot.append(cylinder(pos=(390,3,z),axis =(0,6,0),radius=10 , color = color.red))
-	robot[i].vel = RA.random()	# asigna velocidad
-	robot[i].theta = RA.randint(0,359)
+	x = RA.randint(10,width-10)
+	z = RA.randint(10,height-10)
+	print z
+	robot.append(cylinder(pos=(x,3,z),axis =(0,6,0),radius=10 , color = color.red))
+	robot[i].velocity = RA.random()	# asigna velocidad
+	#robot[i].theta = RA.uniform(0,math.pi*2)	# angulo de movimiento
+	robot[i].theta = math.pi*.5
 	
-	print robot[i].vel
+while 1:
+	rate(100)
+	movRobots()
+	bounce()
